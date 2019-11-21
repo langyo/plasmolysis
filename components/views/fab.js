0896076fdf17cb8abb4e4a9ee5ca13ca579b10fa
langyo <langyo.china@gmail.com>
2019-11-21 19:27:07 +0800

    .

--- components/views/fab.js
+++ components/views/fab.js
@@ -2,10 +2,16 @@ import React from 'react';
 import classnames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
 
-import IconButton from "@material-ui/core/IconButton";
+import {
+  IconButton,
+  Fab
+} from "@material-ui/core";
 
 import Icon from "@mdi/react";
-import { mdiMenu } from "@mdi/js";
+import {
+  mdiMenu,
+  mdiPlus
+} from "@mdi/js";
 
 export default props => {
   const classes = makeStyles(theme => ({
@@ -14,12 +20,21 @@ export default props => {
       left: 30,
       top: 30,
       zIndex: 1000
+    },
+    dashboard: {
+      position: 'absolute',
+      right: 30,
+      bottom: 30,
+      zIndex: 1000
     }
   }))();
 
   return [
     <IconButton className={classes.menu} onClick={() => props.openDrawer()}>
       <Icon path={mdiMenu} size={1} />
-    </IconButton>
+    </IconButton>,
+    <Fab className={classes.dashboard} color='primary'>
+      <Icon path={mdiPlus} size={1} color='#fff' />
+    </Fab>
   ];
 }
