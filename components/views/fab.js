6be02f5c79b3b39a63c83234e35fa885943e58fe
langyo <langyo.china@gmail.com>
2019-11-22 17:59:39 +0800

    .

--- components/views/fab.js
+++ components/views/fab.js
@@ -6,11 +6,17 @@ import {
   IconButton,
   Fab
 } from "@material-ui/core";
+import {
+  SpeedDial,
+  SpeedDialAction
+} from "@material-ui/lab";
 
 import Icon from "@mdi/react";
 import {
   mdiMenu,
-  mdiPlus
+  mdiPlus,
+  mdiPencil,
+  mdiFileOutline
 } from "@mdi/js";
 
 export default props => {
@@ -29,12 +35,32 @@ export default props => {
     }
   }))();
 
+  let [dialOpen, setDialOpen] = React.useState(false);
+
   return [
     <IconButton className={classes.menu} onClick={() => props.openDrawer()}>
       <Icon path={mdiMenu} size={1} />
     </IconButton>,
-    <Fab className={classes.dashboard} color='primary'>
-      <Icon path={mdiPlus} size={1} color='#fff' />
-    </Fab>
+    <SpeedDial
+      ariaLabel="新建"
+      className={classes.dashboard}
+      hidden={false}
+      icon={<Icon path={mdiPlus} size={1} color='#fff' />}
+      onClose={() => setDialOpen(false)}
+      onOpen={() => setDialOpen(true)}
+      open={dialOpen}
+      direction='up'
+    >
+      <SpeedDialAction
+        icon={<Icon path={mdiPencil} size={1} />}
+        tooltipTitle='随笔'
+        onClick={() => setDialOpen(false)}
+      />
+      <SpeedDialAction
+        icon={<Icon path={mdiFileOutline} size={1} />}
+        tooltipTitle='文档'
+        onClick={() => setDialOpen(false)}
+      />
+    </SpeedDial>
   ];
 }
