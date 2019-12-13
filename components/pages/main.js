3df7a2aac55ae59e84982fba8e24146a1975902c
langyo <langyo.china@gmail.com>
2019-12-13 20:28:33 +0800

    . 全局滚动条修改的特别成功！

--- components/pages/main.js
+++ components/pages/main.js
@@ -39,6 +39,11 @@ export default props => {
       position: 'absolute',
       overflow: 'hidden'
     },
+    rootScroll: {
+      width: '100%',
+      height: '100%',
+      overflow: 'auto'
+    },
     centerRow: {
       display: 'flex',
       justifyContent: 'center',
@@ -85,41 +90,41 @@ export default props => {
   }))();
 
   return <div className={classes.root}>
-    {[
-    <div className={classnames(classes.maxWidth, classes.centerRow)}>
-      <Icon path={mdiGreasePencil} size={3} className={classes.margin} color='#3399cc' />
-    </div>,
-    <div className={classnames(classes.maxWidth, classes.centerRow)}>
-      <Typography component={'span'} variant='h5' className={classes.margin}>I 笔记</Typography>
-    </div>,
-    <div className={classnames(classes.maxWidth, classes.centerRow)}>
-      <Paper className={classes.searchBar}>
-        <IconButton className={classes.iconButton} disabled>
-          <Icon path={mdiChevronRight} size={1} />
-        </IconButton>
-        <InputBase
-          className={classes.input}
-          placeholder='搜索'
-        />
-        <IconButton className={classes.iconButton}>
-          <Icon path={mdiMagnify} size={1} />
-        </IconButton>
-      </Paper>
-    </div>,
-    <div className={classnames(classes.maxWidth, classes.centerColumn, classes.cardListWidth, classes.marginTop)}>
-      <ScrollArea mobileNative style={{ width: 400, height: 500 }}>
-        {props.latestPush.length < 1 && <Paper className={classnames(classes.margin, classes.textAlignCenter)}>
-          <Typography variant='body1' className={classes.margin}>{'空空如也'}</Typography>
-          <Typography variant='body1' className={classes.margin}>{'点击右下角的按钮以添加笔记'}</Typography>
-        </Paper>}
-        {props.latestPush.length > 0 && props.latestPush.map((n, index) => <Paper key={index} className={classes.margin}>
-          <Typography variant='h6' className={classes.margin}>{n.title}</Typography>
-          <Typography variant='body1' className={classes.margin}>
-            {redraft(JSON.parse(n.content), renderers)}
-          </Typography>
-        </Paper>)}
-      </ScrollArea>
-    </div>
-    ]}
+    <ScrollArea mobileNative className={classes.rootScroll}>
+      {[
+        <div className={classnames(classes.maxWidth, classes.centerRow)}>
+          <Icon path={mdiGreasePencil} size={3} className={classes.margin} color='#3399cc' />
+        </div>,
+        <div className={classnames(classes.maxWidth, classes.centerRow)}>
+          <Typography component={'span'} variant='h5' className={classes.margin}>I 笔记</Typography>
+        </div>,
+        <div className={classnames(classes.maxWidth, classes.centerRow)}>
+          <Paper className={classes.searchBar}>
+            <IconButton className={classes.iconButton} disabled>
+              <Icon path={mdiChevronRight} size={1} />
+            </IconButton>
+            <InputBase
+              className={classes.input}
+              placeholder='搜索'
+            />
+            <IconButton className={classes.iconButton}>
+              <Icon path={mdiMagnify} size={1} />
+            </IconButton>
+          </Paper>
+        </div>,
+        <div className={classnames(classes.maxWidth, classes.centerColumn, classes.cardListWidth, classes.marginTop)}>
+          {props.latestPush.length < 1 && <Paper className={classnames(classes.margin, classes.textAlignCenter)}>
+            <Typography variant='body1' className={classes.margin}>{'空空如也'}</Typography>
+            <Typography variant='body1' className={classes.margin}>{'点击右下角的按钮以添加笔记'}</Typography>
+          </Paper>}
+          {props.latestPush.length > 0 && props.latestPush.map((n, index) => <Paper key={index} className={classes.margin}>
+            <Typography variant='h6' className={classes.margin}>{n.title}</Typography>
+            <Typography variant='body1' className={classes.margin}>
+              {redraft(JSON.parse(n.content), renderers)}
+            </Typography>
+          </Paper>)}
+        </div>
+      ]}
+    </ScrollArea>
   </div>;
 }
