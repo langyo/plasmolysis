375d73aec180069f794e50452f4137b485097a50
simon3000 <simon30002021@icloud.com>
2019-12-22 14:58:16 +0800

    to for or not to for

--- src/services.js
+++ src/services.js
@@ -1,30 +1,22 @@
+/* eslint-disable semi */
 import context from '../../../server/context';
 import config from '../../../configs/config';
 
 import { readdirSync, statSync } from 'fs';
+import { join } from 'path'
 
-const fileReadDir = name => {
-  let files = readdirSync(name);
-  let ret = {};
-
-  for (let file of files) {
-    if (statSync(`${name}/${file}`).isDirectory())
-      ret[file] = fileReadDir(`${name}/${file}`);
-    else ret[file.slice(0, file.length - 3)] = require(`${name}/${file}`).default;
-  }
-
-  return ret;
-};
+const fileReadDir = name => Object.fromEntries(readdirSync(name)
+  .map(file => [file, statSync(join(name, file)).isDirectory()])
+  .map(([file, isDirectory]) => isDirectory ? [file, fileReadDir(join(name, file))] : [file.slice(0, file.length - 3), require(join(name, file)).default]))
 
 const controllers = fileReadDir('../../../controllers/');
-let pagePreload = {};
+const pagePreload = {};
 
-let services = {};
-
-for (let type of ['models', 'pages', 'views']) {
-  for (let name of Object.keys(controllers[type])) {
+const services = Object.fromEntries(['models', 'pages', 'views']
+  .map(type => [type, controllers[type]])
+  .flatMap(([type, controller]) => Object.entries(controller).flatMap(([name, action]) => {
     // 生成动作表
-    let dealed = controllers[type][name]({
+    const dealed = action({
       deal: () => ({ type: 'deal' }),
       togglePage: () => ({ type: 'togglePage' }),
       createModel: () => ({ type: 'createModel' }),
@@ -41,84 +33,61 @@ for (let type of ['models', 'pages', 'views']) {
     });
 
     // 去除所有的不用于表达动作的特殊键
-    if(type === 'pages') pagePreload[name] = dealed.preLoad || (async () => ({}));
-    dealed = Object.keys(dealed)
-      .filter(name => ['init', 'preLoad'].indexOf(name) < 0)
-      .reduce((prev, next) => ({ ...prev, [next]: dealed[next] }), {});
-
-    // 对其中每个作为数组存在的元素进行扁平化
-    dealed = Object.keys(dealed).reduce((prev, action) => {
-      const dfs = arr => arr.reduce(
-        (prev, next) => Array.isArray(next) ? prev.concat(dfs(next)) : [...prev, next],
-        []
-      );
-      return ({
-        ...prev,
-        [action]: dfs(dealed[action])
-      });
-    }, {});
-
-    // 整合所有的 fetch
-    dealed = Object.keys(dealed).reduce((prev, action) => {
-      if (dealed[action]) {
-        return ({
-          ...prev,
-          [action]: Object.keys(dealed[action]).reduce((prev, next) => {
-            if (dealed[action][next]) {
-              switch (dealed[action][next].type) {
-                case 'fetch':
-                case 'send':
-                  return prev;
-                case 'route':
-                  return { ...prev, fetchObj: { ...prev.fetchObj, route: dealed[action][next].obj } };
-                case 'handle':
-                  return { list: [...prev.list, { type: 'fetchCombine', ...prev.fetchObj, handle: dealed[action][next].func }], fetchObj: {} };;
-                default:
-                  return { ...prev, list: [...prev.list, dealed[action][next]] };
-              }
-            } else return prev;
-          }, { list: [], fetchObj: {} }).list
-        });
-      }
-      else return prev;
-    }, {});
-
-    for (let action of Object.keys(dealed)) {
-      for (let task of dealed[action]) {
-        switch (task.type) {
-          case 'fetchCombine':
-            console.log('New service:', task.route.path);
-            services[task.route.path] = context => (req, res) => task.handle(req.body, context, json => {
-              res.send(JSON.stringify(json));
-              res.end();
-            });
-            break;
-          default:
-            break;
-        }
-      }
-    }
-  }
-}
+    if (type === 'pages') pagePreload[name] = dealed.preLoad || (async () => ({}));
+    return Object.entries(dealed)
+      .filter(([action]) => !['init', 'preLoad'].includes(action))
+      // 对其中每个作为数组存在的元素进行扁平化
+      .map(([name, action]) => [name, action.flat(Infinity)])
+      // 整合所有的 fetch
+      .filter(([_action, f]) => Boolean(f))
+      .map(([action, f]) => [action, Object.values(f)
+        .filter((fValue) => Boolean(fValue))
+        .filter((fValue) => !['fetch', 'send'].includes(fValue.type))
+        .reduce(({ fetchObj, list }, fValue) => {
+          switch (fValue.type) {
+            case 'route':
+              fetchObj.route = fValue.obj;
+              break;
+            case 'handle':
+              list.push({ type: 'fetchCombine', ...fetchObj, handle: fValue.func });
+              fetchObj = {};
+              break;
+            default:
+              list.push(fValue);
+          }
+          return { fetchObj, list }
+        }, { list: [], fetchObj: {} })
+        .list
+      ])
+      .map(([_action, actionValue]) => actionValue)
+      .flatMap(actionValue => actionValue
+        .filter(task => task.type === 'fetchCombine')
+        .map(task => {
+          console.log('New service:', task.route.path);
+          return [task.route.path, context => (req, res) => task.handle(req.body, context, json => {
+            res.send(JSON.stringify(json));
+            res.end();
+          })]
+        }))
+  })));
 
 export default server => {
-  for (let path of Object.keys(services)) {
-    server.use(path, (req, res) => services[path](context)(req, res));
-  }
+  Object.entries(services).forEach(([path, service]) => server.use(path, (req, res) => service(context)(req, res)))
 
-  for (let page of Object.keys(pagePreload)) {
-    console.log(`New preload service: ${page}`);
-    server.use(`/preload/${page}`, (req, res) => {
-      pagePreload[page](context, req.cookies).then(data => {
-        res.send(JSON.stringify({
-          data,
-          cookies: {
-            ...config.initCookies,
-            ...req.cookies
-          }
-        }));
-        res.end();
+  Object.entries(pagePreload)
+    .map(([page, callback]) => {
+      console.log(`New preload service: ${page}`);
+      server.use(`/preload/${page}`, (req, res) => {
+        callback(context, req.cookies).then(data => {
+          res.send(JSON.stringify({
+            data,
+            cookies: {
+              ...config.initCookies,
+              ...req.cookies
+            }
+          }));
+          res.end();
+        });
       });
-    });
-  }
+    })
 }
