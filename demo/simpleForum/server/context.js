8a66c30d6ba8ea06fa9f0505a7b7584ea10af21e
langyo <langyo.china@gmail.com>
2020-01-04 13:46:11 +0800

    .

--- demo/simpleForum/server/context.js
+++ demo/simpleForum/server/context.js
@@ -7,10 +7,11 @@ import {
   mkdirSync as mkdir
 } from 'fs';
 import { resolve } from 'path';
+import shortid from 'shortid';
 
-if(!exist(resolve('./data'))) mkdir('./data');
-if(!exist(require('./data/users'))) mkdir('./data/users');
-if(!exist(require('./data/threads'))) mkdir('./data/threads');
+if (!exist(resolve('./data'))) mkdir('./data');
+if (!exist(require('./data/users'))) mkdir('./data/users');
+if (!exist(require('./data/threads'))) mkdir('./data/threads');
 
 let users = Array.prototype.slice.call(
   readDir(resolve('./data/users'))
@@ -19,7 +20,7 @@ let users = Array.prototype.slice.call(
     .map(body => JSON.parse(body))
     .filter(obj => typeof obj.id === 'number' && typeof obj.name === 'string')
     .reduce((obj, user) => ({
-      ..obj,
+      ...obj,
       [user.id]: user
     }), {})
 );
@@ -28,8 +29,8 @@ let threads = Array.prototype.slice.call(
   readDir(resolve('./data/threads'))
     .filter(name => /\.json$/.test(name))
     .map(name => readFile(resolve('./data/threads', name), 'utf8'))
-    .map(body => JSON.parse(body))           .filter(obj => typeof obj.id === 'number' && typeof obj.owner === 'number')        .reduce((obj, thread) => ({
-      ..obj,
+    .map(body => JSON.parse(body)).filter(obj => typeof obj.id === 'number' && typeof obj.owner === 'number').reduce((obj, thread) => ({
+      ...obj,
       [thread.id]: thread
     }), {})
 );
@@ -37,12 +38,13 @@ let threads = Array.prototype.slice.call(
 let threadsId = [];
 const sortThreads = () => threadsId = [...threads].sort((a, b) => a.onTop || !b.onTop || a.date > b.date);
 
-if(!exist(resolve('./data/configs.json')) {
-  if(!exist(resolve('./data/users/1.json')) {
+if (!exist(resolve('./data/configs.json'))) {
+  if (!exist(resolve('./data/users/1.json'))) {
     users[1] = {
       id: 1,
       name: 'admin',
-      password: 'admin', // 不再需要 manager 这个项，因为 config.json 下有了
+      password: 'admin',
+      // 不再需要 manager 这个项，因为 config.json 下有了
       banned: false,
       replys: [],
       notices: []
@@ -62,4 +64,25 @@ const writeUser = id => writeFile(JSON.stringify(users[id]), resolve('./data/use
 const writeThread = id => writeFile(JSON.stringify(threads[id]), resolve('./data/threads', `${id}.json`), 'utf8');
 const writeConfig = () => writeFile(JSON.stringify(configs), resolve('./data/configs.json'), 'utf8');
 
-// 待录入
+export const login = (inName, password) => {
+  for (let { name } of users) {
+    if (inName === name) return { result: 'fail', reason: 'The name has been used.' };
+  }
+  let id = users.length;
+  users.push({
+    id,
+    name: inName,
+    password,
+    banned: false,
+    accessKey: shortid.generate()
+  });
+  writeUser(id);
+  return { result: 'true', accessKey: users[id].accessKey };
+};
+
+export const reply = (accessKey, userId, threadId, content, referTo) => {
+  if(!(users[userId] && users[userId].accessKey === accessKey && !users[userId].banned)) return { result: 'fail', reason: 'Error user token.' };
+  if(!(threads[threadId] && !threads[threadId].hasClosed)) return { result: 'fail', reason: 'Error thread.' };
+  if((!referTo) || (!users[referTo])) return { result: 'fail', reason: 'Unknown reference.' };
+  // 待录入，页码 3
+};
--- src/utils/fileUtil.js
+++ src/utils/fileUtil.js
@@ -1,5 +1,6 @@
 import { readdirSync, statSync, watch } from 'fs';
 import { resolve } from 'path';
+import { EventEmitter } from 'events';
 
 // 功能为初始化项目而进行完整扫描，并提供文件路径对应
 export const scanDir = src => {
@@ -12,7 +13,8 @@ export const scanDir = src => {
       else ret[`${path}.${name}`] = resolve(src, file);
     }
   };
-  for (let file of readdirSync(src)) {       const name = /^(.+)\.js$/.exec(file)[1];
+  for (let file of readdirSync(src)) {
+    const name = /^(.+)\.js$/.exec(file)[1];
     if (statSync(resolve(src, file)).isDirectory()) dfs(resolve(src, file), `${name}`);
     if (!name) continue;
     else ret[name] = resolve(src, file);
@@ -21,33 +23,51 @@ export const scanDir = src => {
 }
 
 // 功能为监视文件夹，包括新建、删除与更新文件
-export const watchDir = (src, callback) => {
-  const dfs;
-  // Developing
-  for (let file of readdirSync(src)) {
-    if (statSync(resolve(src, file)).isDirectory()) {
-      let subFiles = '';
-      fs.watch(`${src}/${file}/`, (type, name) => {
-        if (type === 'rename') {
-          if(subFiles.indexOf(name) < 0) {
-            // New file.
-            subFiles.push(name);
-            callback(`${src}/${file}/${name}`, 'create', `${path}.${name}`.substr(1));
+export const watchDir = src => {
+  let emitter = new EventEmitter();
+  const dfs = (src, path) => {
+    let files = readdirSync(src);
+    watch(src, (type, file) => {
+      if (statSync(resolve(src, file)).isDirectory()) {
+        if (file[0] !== '.' && type === 'rename') {
+          if (files.indexOf(file) < 0) {
+            dfs(resolve(src, file), `${path}.${file}`);
+            files.push(file);
           } else {
-            // Delete File.
-            subFiles.splice(subFiles.indexOf(name), subFiles.indexOf(name) + 1);
-            callback(`${src}/${file}/${name}`, 'delete', `${path}.${name}`.substr(1));
+            files = files.splice(files.indexOf(file), 1);
           }
         }
-      })
-    }
-    else {
-      fs.watchFile(`${src}/${file}`, (cur, prev) => {
-        callback(`${src}/${file}`, 'update', `${path}.${file}`.substr(1));
+      } else {
+        if (!/^(.+)\.js$/.test(file)) return;
+        const name = /^(.+)\.js$/.exec(file)[1];
+        if (type === 'rename') {
+          if (files.indexOf(file) < 0) {
+            // Create a new file.
+            files.push(file);
+            emitter.emit('create', `${path}.${name}`, resolve(src, file));
+          } else {
+            // Delete the file.
+            files = files.splice(files.indexOf(file), 1);
+            emitter.emit('delete', `${path}.${name}`);
+          }
+        } else {
+          // Update the file.
+          emitter.emit('update', `${path}.${name}`, resolve(src, file));
+        }
+      }
+    });
+  };
+  for (let file of files) {
+    if (statSync(resolve(src, file)).isDirectory()) {
+      dfs(resolve(src, file), file);
+    } else {
+      if (!/^(.+)\.js$/.test(file)) continue;
+      const name = /^(.+)\.js$/.exec(file)[1];
+      watch(resolve(src, file), type => {
+        if (type === 'update') emitter.emit('update', name, resolve(src, file));
       });
-      callback(`${src}/${file}`, 'init', `${path}.${file}`.substr(1));
     }
   }
-  return files;
+  return emitter;
 };
 
