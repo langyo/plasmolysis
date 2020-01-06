f9128ecbeed44af76e0def6770b0054e2eee583f
langyo <langyo.china@gmail.com>
2020-01-06 21:48:47 +0800

    准备抛弃 nextjs，自行构建服务端部分

--- README.md
+++ README.md
@@ -5,3 +5,41 @@
 该框架仍处于试验研发阶段，未来将以发布至 NPM 的形式供广大程序员使用。
 
 当前开发的版本暂时为服务端 - 客户端模式的框架，未来计划将会同时支持 SPA 应用缓存与本地应用的开发（分别使用 Electron 与 Cordova）。
+
+## 运行示例
+
+> 请注意，该仓库目前仍处于不稳定状态，随时可能发生大的更改；另外，处于 dev 分支的工程甚至可能无法正常运行。
+>
+> 如需运行稳定版本，请切换到 master 分支再运行。
+>
+> 请确保您的机器上已经安装了 Git、NodeJS；如需运行示例应用，还需要安装 MongoDB，并请确保 MongoDB 的服务正在运行。
+
+1. 克隆本仓库
+
+```shell
+git clone https://github.com/langyo/nickelcat
+# 如需运行稳定版本，请运行下一条命令，切换到 master 分支
+git checkout master
+# 如需运行开发版本，请运行下一条命令，切换到 dev 分支
+git checkout dev
+```
+
+2. 安装依赖
+
+```shell
+npm install
+# 如需运行示例应用，请运行下面一批指令，以同时解决示例应用的依赖
+cd demo/template
+npm install
+```
+
+3. 运行
+
+```shell
+# 如尚未进入示例应用文件夹，请先进入该文件夹
+cd demo/template
+# 运行
+npm run serve
+# 运行（开发者模式）
+npm start
+```
--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,17 @@
 import express from 'express';
 import compression from 'compression';
-import next from 'next';
 import helmet from 'helmet';
 import { configure, connectLogger, getLogger, levels } from 'log4js';
 import { json } from 'body-parser';
 
+import ReactDom from 'react-dom';
 import { resolve } from 'path';
 
 const port = parseInt(process.env.PORT, 10) || 80;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
 
 import services from './services';
-import { scanDir } from '../utils/fileUtil';
 import { workDirPath, fileEmitter } from '../utils/require';
-
-// TODO: It will be updated when the folder has something change.
-const pages = Object.keys(scanDir(resolve(workDirPath, './controllers/pages')));
 
 app.prepare().then(() => {
   const server = express();
@@ -46,14 +40,7 @@ app.prepare().then(() => {
 
   server.use(json());
 
-  services(server);
-
-  server.get('/', (req, res) => app.render(req, res, resolve(`../../pages/index.js`), req.query));
-  server.get('/index', (req, res) => app.render(req, res, resolve(`../../pages/index.js`), req.query));
-
-  for(let name of pages) {
-    server.get(`/${name}`, (req, res) => app.render(req, res, resolve(`../../pages/index.js`), req.query));
-  }
+  // services(server);
 
   server.get('*', (req, res) => {
     return handle(req, res);
