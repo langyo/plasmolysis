b0764a6f4fa97c17a00a1fdd0dd33477542cb692
langyo <m13776491897@163.com>
2020-08-28 21:44:04 +0800

    :factory: Will abandon the memory fs.

--- packages/dev-server/webpackLoader.ts
+++ packages/dev-server/webpackLoader.ts
@@ -2,76 +2,204 @@ import * as webpack from 'webpack';
 import * as TerserPlugin from 'terser-webpack-plugin';
 import { createConfigItem } from '@babel/core';
 
-import { scan } from './projectWatcher';
 import { join } from 'path';
 
-export async function compile(
+import * as realFs from 'fs';
+
+async function scan(
+  parseFilterComponents: string[]
+    = ['', 'dialog.', 'dialogs.', 'page.', 'pages.', 'view.', 'views.']
+): Promise<typeof realFs> {
+  let components: {
+    name: string,
+    path: string
+  }[] = [];
+  let configPath: string = '';
+
+  const scanDfs = (path: string, route: string = '') => {
+    let list: {
+      fileName: string,
+      path: string,
+      route: string
+    }[] = [];
+    for (const fileName of realFs.readdirSync(path)) {
+      if (realFs.statSync(join(path, fileName)).isDirectory()) {
+        list = list.concat(
+          scanDfs(join(path, fileName), `${route}${fileName}.`)
+        );
+      }
+      else if (/(\.js)|(\.mjs)|(\.ts)|(\.jsx)|(\.tsx)$/.test(fileName)) {
+        list.push({
+          fileName: `${route}${fileName.slice(0, fileName.lastIndexOf('.'))}`,
+          path: join(path, fileName),
+          route
+        });
+      }
+    }
+    return list;
+  }
+
+  if (
+    realFs.existsSync(join(process.cwd(), './src')) &&
+    realFs.statSync(join(process.cwd(), './src')).isDirectory()
+  ) {
+    for (const component of scanDfs(join(process.cwd(), './src'))) {
+      if (parseFilterComponents.indexOf(component.route) >= 0) {
+        components.push({
+          name: component.fileName,
+          path: component.path.split('\\').join('\\\\')
+        });
+        break;
+      }
+    }
+  }
+
+  configPath =
+    realFs.existsSync(
+      join(process.cwd(), './nickelcat.config.ts')
+    ) && join(process.cwd(), './nickelcat.config.ts') ||
+    realFs.existsSync(
+      join(process.cwd(), './nickelcat.config.js')
+    ) && join(process.cwd(), './nickelcat.config.js');
+
+  const virtualFiles = {
+    ['/__nickelcat_staticRequire.js']: `
+module.exports = ${
+      JSON.stringify({
+        webClient: {
+          ...components.reduce((obj, { name, path }) => ({
+            ...obj,
+            [name]: {
+              component: `require("${
+                path.split('\\').join('\\\\')
+                }").default`,
+              controller: `require("${
+                path.split('\\').join('\\\\')
+                }").controller`
+            }
+          }), {})
+        },
+        nodeServer: {
+          // TODO - Upgrade the structure.
+        }
+      })};`,
+    ['/__nickelcat_defaultClientLoader.js']:
+      `require("${
+      join(__dirname, './defaultClientLoader.js').split('\\').join('\\\\')
+      }")`,
+    ['/__nickelcat_defaultServerLoader.js']:
+      `require("${
+      join(__dirname, './defaultServerLoader.js').split('\\').join('\\\\')
+      }")`
+  };
+
+  // TODO - Abandon the memory fs instead of some hooks.
+  let fs = {
+    ...realFs,
+    join,
+    read: (...args: any[]) =>
+      (console.log('read', args),
+        realFs.readFile.apply(undefined, args)),
+    readlink: (...args: any[]) =>
+      (console.log('readlink', args),
+        realFs.readFile.apply(undefined, args)),
+    readlinkSync: (...args: any[]) =>
+      (console.log('readlinkSync', args),
+        realFs.readFile.apply(undefined, args)),
+    readv: (...args: any[]) =>
+        (console.log('readv', args),
+          realFs.readFile.apply(undefined, args)),
+    readvSync: (...args: any[]) =>
+      (console.log('readvSync', args),
+        realFs.readFile.apply(undefined, args)),
+    readFile: (...args: any[]) =>
+      (console.log('readFile', args),
+        realFs.readFile.apply(undefined, args)),
+    readFileSync: (...args: any[]) =>
+      (console.log('readFileSync', args),
+        realFs.readFile.apply(undefined, args)),
+    readdir: (...args: any[]) =>
+      (console.log('readdir', args),
+        realFs.readFile.apply(undefined, args)),
+    readdirSync: (...args: any[]) =>
+      (console.log('readdirSync', args),
+        realFs.readFile.apply(undefined, args)),
+    stat: (...args: any[]) =>
+      (console.log('stat', args),
+        realFs.readFile.apply(undefined, args)),
+    statSync: (...args: any[]) =>
+      (console.log('statSync', args),
+        realFs.readFile.apply(undefined, args))
+  }
+
+  return fs as any;
+};
+
+export async function generateCompiler(
   webpackConfig: { [key: string]: any }
 ): Promise<() => Promise<{ code: string, sourceMap: string }>> {
-  const fs = await scan();
-
-  const compiler = webpack({
-    mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-    context: process.cwd(),
-    module: {
-      rules: [
-        {
-          test: /\.[jt]sx?$/,
-          loader: 'babel-loader',
-          exclude: /node_modules/,
-          options: {
-            presets: [
-              createConfigItem(
-                require(join(__dirname, './node_modules/@babel/preset-env'))
-              ),
-              createConfigItem(
-                require(join(__dirname, './node_modules/@babel/preset-react'))
-              ),
-              createConfigItem(
-                require(join(__dirname, './node_modules/@babel/preset-typescript'))
-              )
-            ]
+  return async function (): Promise<{ code: string, sourceMap: string }> {
+    const fs = await scan();
+    const compiler = webpack({
+      mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+      context: process.cwd(),
+      module: {
+        rules: [
+          {
+            test: /\.[jt]sx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+              presets: [
+                createConfigItem(
+                  require(join(__dirname, './node_modules/@babel/preset-env'))
+                ),
+                createConfigItem(
+                  require(join(__dirname, './node_modules/@babel/preset-react'))
+                ),
+                createConfigItem(
+                  require(join(__dirname, './node_modules/@babel/preset-typescript'))
+                )
+              ]
+            }
           }
-        }
-      ]
-    },
-    resolve: {
-      modules: [
-        join(__dirname, './node_modules'),
-        join(process.cwd(), './node_modules'),
-        'node_modules'
-      ]
-    },
-    resolveLoader: {
-      modules: [
-        join(__dirname, './node_modules'),
-        join(process.cwd(), './node_modules'),
-        'node_modules'
-      ]
-    },
-    output: {
-      filename: 'output.js',
-      path: '/'
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin({
-        terserOptions: {
-          output: {
-            comments: false
+        ]
+      },
+      resolve: {
+        modules: [
+          join(process.cwd(), './node_modules'),
+          'node_modules'
+        ]
+      },
+      resolveLoader: {
+        modules: [
+          join(__dirname, './node_modules'),
+          join(process.cwd(), './node_modules'),
+          'node_modules'
+        ]
+      },
+      output: {
+        filename: 'output.js',
+        path: '/'
+      },
+      optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+          terserOptions: {
+            output: {
+              comments: false
+            },
           },
-        },
-        extractComments: false,
-        sourceMap: process.env.NODE_ENV === 'development'
-      })],
-    },
-    devtool: 'source-map',
-    ...webpackConfig
-  });
-  compiler.inputFileSystem = fs;
-  compiler.outputFileSystem = fs as any;
+          extractComments: false,
+          sourceMap: process.env.NODE_ENV === 'development'
+        })],
+      },
+      devtool: 'source-map',
+      ...webpackConfig
+    });
+    compiler.inputFileSystem = fs;
+    compiler.outputFileSystem = fs as any;
 
-  return function compile(): Promise<{ code: string, sourceMap: string }> {
     return new Promise((resolve, reject) => {
       compiler.run((err: Error, status) => {
         if (err) {
@@ -92,8 +220,10 @@ export async function compile(
 
         else {
           resolve({
-            code: fs.readFileSync('/output.js').toString(),
-            sourceMap: fs.readFileSync('/output.js.map').toString()
+            code:
+              fs.readFileSync('/output.js', { encoding: 'utf8' }).toString(),
+            sourceMap:
+              fs.readFileSync('/output.js.map', { encoding: 'utf8' }).toString()
           });
         }
       })
