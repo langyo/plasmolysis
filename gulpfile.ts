1d05b3f3db2510ccd059435d9ca72a90bf5ff2c2
langyo <langyo.china@gmail.com>
2020-09-04 08:50:05 +0800

    :bug: Fix Gulp task.

--- gulpfile.ts
+++ gulpfile.ts
@@ -219,7 +219,7 @@ export const publish = series(
 
       // Create a version tag.
       return spawn(
-        process.platform === 'win32' ? `git.cmd` : 'git',
+        'git',
         ['tag', '-a', `v${major}.${minor}.${patch}`, '-m', `v${major}.${minor}.${patch}`],
         { stdio: 'inherit', cwd: process.cwd() }
       );
