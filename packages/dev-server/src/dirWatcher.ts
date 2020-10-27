66b8ae51fea74ef07be26a146fa7dd85060a0b29
langyo <langyo.china@gmail.com>
2020-10-27 22:52:19 +0800

    :cookie: Add the route path and more. Make the scanner and watcher more stronger.

--- packages/dev-server/src/dirWatcher.ts
+++ packages/dev-server/src/dirWatcher.ts
@@ -3,19 +3,22 @@ import * as realFs from 'fs';
 import { join } from 'path';
 
 export function dirScanner(path: string = process.cwd()) {
-  function dfs(path: string, route: string = '.') {
+  function dfs(path: string, route: string = '') {
     let nodes: { path: string, route: string }[] = [];
     for (const fileName of realFs.readdirSync(path)) {
       if (realFs.statSync(join(path, fileName)).isDirectory()) {
         nodes.concat(dfs(join(path, fileName), `${route}.${fileName}`));
       }
       else if (/(\.js)|(\.mjs)|(\.ts)|(\.jsx)|(\.tsx)$/.test(fileName)) {
-        nodes.push({ path: join(path, fileName), route: route.substr(1) });
+        nodes.push({
+          path: join(path, fileName),
+          route: /^(.+)(\.[a-z]+)$/.exec(`${route}.${fileName}`.substr(1))[1]
+        });
       }
     }
     return nodes;
   }
-  return Object.keys(realFs.readdirSync(path)).reduce((obj, key) => ({
+  return realFs.readdirSync(path).reduce((obj, key) => ({
     ...obj,
     [key]: dfs(join(path, `./${key}`))
   }), {});
@@ -23,14 +26,16 @@ export function dirScanner(path: string = process.cwd()) {
 
 export async function dirWatcher(
   path: string,
-  callback: (diffPaths: { path: string, type: string }[]) => Promise<void>
+  callback: (diffPaths: {
+    path: string, type: string, route: string
+  }[]) => Promise<void>
 ): Promise<void> {
   // The flag that the timer is running.
   let delayWaiting: boolean = false;
   // The flag that the files were changed when the timer was running.
   let changedDuringDelay: boolean = false;
   // The list for the files that have been changed.
-  let diffList: { path: string, type: string }[] = [];
+  let diffList: { path: string, type: string, route: string }[] = [];
 
   async function delayUpdate() {
     delayWaiting = true;
@@ -48,13 +53,27 @@ export async function dirWatcher(
 
   watchFiles(path, {
     ignored: /(node_modules)|(\.git)/
-  }).on('all', async (type, path) => {
+  }).on('all', async (type, filePath) => {
+    if (['add', 'change', 'unlink'].indexOf(type) < 0) {
+      return;
+    }
+
+    const route = /^(.+)(\.[a-z]+)$/.exec(filePath
+      .substr(path.length + 1)
+      .split(/[\\\/]/)
+      .splice(1)
+      .join('.'))[1];
+
     if (delayWaiting) {
       changedDuringDelay = true;
-      diffList.push({ type, path });
+      diffList.push({
+        type, path: filePath, route
+      });
     }
     else {
-      diffList.push({ type, path });
+      diffList.push({
+        type, path: filePath, route
+      });
       await delayUpdate();
     }
   });
