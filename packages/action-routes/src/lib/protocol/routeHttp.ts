fdb7b7bac11cccdef37033db7607df6a4d50c17f
langyo <langyo.china@gmail.com>
2020-12-07 23:54:47 +0800

    :cookie: Add the arguments for protocol routers.

--- packages/action-routes/src/lib/protocol/routeHttp.ts
+++ packages/action-routes/src/lib/protocol/routeHttp.ts
@@ -2,6 +2,13 @@ import { IRuntimeObject } from 'nickelcat';
 import { IInitArgs } from 'nickelcat-action-preset';
 
 export function routeHttp(
+  entry: string | {
+    host?: string,
+    port?: string,
+    path: string,
+    security?: 'httpOnly' | 'httpsOnly' | 'auto'
+  },
+  action: IRuntimeObject
 ): IRuntimeObject {
   return {
     type: 'preset.routeHttp',
--- packages/action-routes/src/lib/protocol/routeWebSocket.ts
+++ packages/action-routes/src/lib/protocol/routeWebSocket.ts
@@ -2,6 +2,11 @@ import { IRuntimeObject } from 'nickelcat';
 import { IInitArgs } from 'nickelcat-action-preset';
 
 export function routeWebSocket(
+  port: number | {
+    port: number,
+    headParser?: (headStr: string, variants: IInitArgs) => string | undefined,
+    security?: 'wsOnly' | 'wssOnly' | 'auto'
+  }
 ): IRuntimeObject {
   return {
     type: 'preset.routeWebSocket',
