ce50fa0c573ac150cd4fa41ce5d91a09f9579159
langyo <langyo.china@gmail.com>
2020-12-11 13:39:17 +0800

    :cookie: Add a global entry.

---
+++ packages/dev-server/src/actionTraitParser.ts
@@ -0,0 +1,15 @@
+import { IActionObject } from 'nickelcat'
+import { installComponent } from './frontendLoader';
+import { installRoute } from './backendLoader';
+
+export function parseActionObject(
+  routePath: string, actionName: string, obj: IActionObject
+) {
+
+}
+
+export function parseAST(
+  routePath: string, actionName: string, tree: unknown
+) {
+
+}
