bd8bd21b368c3f8e7e7684ce874ce7af8088ae96
langyo <langyo.china@gmail.com>
2020-12-20 20:09:12 +0800

    :factory: Use JSX tag instead of function call. Now it is still refactoring.

--- packages/dev-server/src/index.ts
+++ packages/dev-server/src/index.ts
@@ -1,5 +1,9 @@
 export type IEnv =
   'browser' | 'server' | 'native-electron' | 'native-cordova';
+export const envMap =
+  ['browser', 'server', 'native-electron', 'native-cordova'];
+
 export type IProtocol =
   'http' | 'https' | 'ws' | 'wss' | 'iframe-post' | 'ipc';
-
+export const protocolMap =
+  ['http', 'ws', 'iframe-post', 'ipc'];
--- packages/dev-server/src/sourceCodeTraverser.ts
+++ packages/dev-server/src/sourceCodeTraverser.ts
@@ -1,81 +1,106 @@
-import { IEnv } from './index';
+import { IEnv, IProtocol, envMap, protocolMap } from './index';
 import { parse } from '@babel/parser';
 import { traverse } from '@babel/traverse';
 import { generate } from '@babel/generator';
 import * as t from '@babel/types';
+import { join } from 'path';
 
-export function parseSourceCode(
-  envTag: IEnv, routePath: string, sourceCode: string
-): { [env: string]: { [path: string]: { code: string, map: string } } } {
-  let importNodes: string[] = [];
-  let importSpecSet: string[] = [];
-  let specActionMap = {
-    on: 'on',
-    to: 'to'
+export type ISourceMap = {
+  [env in IEnv]?: {
+    [path: string]: { code: string; map: string; };
   };
+};;
 
-  traverse(
-    parse(sourceCode, {
-      sourceType: 'module',
-      plugins: ['jsx', 'typescript']
-    }), {
-      ImportDeclaration({ node }) {
-        importNodes.push(node);
+export function traverser(
+  envTag: IEnv,
+  routePath: string,
+  sourceCode: string,
+  actionList: { [path: string]: string[] }
+): ISourceMap {
+  let ret: ISourceMap = { [envTag]: {} };
+  let actionSet: string[] = [];
 
-        if (isActionPackage(node.source.value)) {
-          traverse(node, {
-            ImportSpecifier({ node }) {
-              importSpecSet.push(node.local.value);
-            } // TODO - Will support the other way.
-          });
+  function dfs(ast: t.BlockStatement) {
+    return traverse(ast, {
 
-          if (node.source.value === 'nickelcat-action-preset') {
-            traverse(node, {
-              ImportSpecifier({ node }) {
-                switch(node.imported.value) {
-                  case 'on':
-                    specActionMap.on = node.local.value;
-                    break;
-                  case 'to':
-                    specActionMap.to = node.local.value;
-                    break;
-                  default:
-                    break;
-                }
-              }
-            });
-          }
-        }
-      },
-      ExportNamedDeclaration({ node, remove }) {
-        if (
-          t.isVariableDeclaration(node.declaration) &&
-          t.isCallExpression(node.declaration.declarations[0])
-        ) {
-          if (t.isStringLiteral(node.declaration.declarations[0].callee, {
-            value: specActionMap.on
-          })) {
-            if (t.isStringLiteral(
-              node.declaration.declarations[0].arguments[0]
-            )) {
-              if (
-                node.declaration.declarations[0].arguments[0].value === envTag
-              ) {
-                node.declaration.declarations[0].replaceWith(
-                  t.asyncFunction();
-                );
-              } else {
-                remove();
-              }
-            } else {
-              throw node.declaration.declarations[0].buildFrameError(
-                'The first argument must be a string.'
-              );
-            }
+    });
+  }
+
+  ret[envTag][routePath] = traverse(parse(sourceCode, {
+    sourceType: 'module',
+    plugins: ['jsx', 'typescript']
+  }), {
+    ImportDeclaration(path) {
+      if (
+        Object.keys(actionList).indexOf(
+          join(path.get('source').value)
+        ) >= 0
+      ) {
+        for (const n of path.get('specifiers')) {
+          if (
+            actionList[join(path.get('source').value)].indexOf(
+              n.get('imported').name)
+          ) {
+            actionSet.push(n.get('local').value);
           }
         }
       }
+    },
+    JSXElement(path) {
+      if (path.get('openingElement.name').name === 'on') {
+        const { name: envFromExport } =
+          path.get('openingElement.attributes.0.name');
+        if (envMap.indexOf(envFromExport) < 0) {
+          path.get('openingElement.attributes.0.name').buildFrameError(
+            `The environment is not support: '${envFromExport}'`
+          );
+        }
+        if (!path.get('children.0.expression').isFunction()) {
+          path.get('children.0.expression').buildFrameError(
+            `The children must be a function.`
+          );
+        }
+        traverse(path.get('children.0.expression'), {
+          JSXElement(path) {
+            if (path.get('openingElement.name').name === 'on') {
+              throw path.get('openingElement.name').buildFrameError(
+                `Not allowed to nest again within an environment declaration.`
+              );
+            }
+          }
+        });
+        if (envFromExport === envTag) {
+          path.replaceWith(path.get('children.0.expression.body'));
+        }
+      } else {
+        throw path.buildFrameError('Only support the tag "on".');
+      }
+    },
+    ExportNamedDeclaration(path) {
+      let n = path.get('declaration.declrations.0.init');
+      if (n.isJSXElement()) {
+        if (n.get('openingElement.name').name === 'on') {
+          const { name: envFromExport } =
+            n.get('openingElement.attributes.0.name');
+          if (envMap.indexOf(envFromExport) < 0) {
+            n.get('openingElement.attributes.0.name').buildFrameError(
+              `The environment is not support: '${envFromExport}'`
+            );
+          }
+          if (!n.get('children.0.expression').isFunction()) {
+            n.get('children.0.expression').buildFrameError(
+              `The children must be a function.`
+            );
+          }
+          if (envFromExport === envTag) {
+            n.replaceWith(dfs(n.get('children.0.expression.body')));
+          }
+        } else {
+          throw path.buildFrameError('Only support the tag "on".');
+        }
+      }
     }
-  }
-}
+  });
 
+  return ret;
+}
