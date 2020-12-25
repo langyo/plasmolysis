61d742a7fd4acdbd081b0a14a7a27fa3243de8b4
langyo <langyo.china@gmail.com>
2020-12-25 15:07:13 +0800

    :factory: Create 'worker' entity instead of 2/3D. This is a more general solution.

---
+++ packages/action-preset/src/worker.ts
@@ -0,0 +1,13 @@
+export const requiredItems = ['worker'];
+
+export function constructor(
+  pkg: { [key: string]: unknown },
+  variants: { [key: string]: unknown }
+): string {
+  // TODO - Returns the variants' generator id.
+  return '';
+}
+
+export function variantsGenerator(id: string) {
+
+}
