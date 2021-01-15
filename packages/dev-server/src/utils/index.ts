37ada03b4ab2654a07437f919cfecc9b5d5bd4ea
langyo <langyo.china@gmail.com>
2021-01-15 23:16:29 +0800

    :factory: Move some files.

--- packages/dev-server/src/utils/index.ts
+++
@@ -1,7 +0,0 @@
-export { exprVerify } from './exprVerify';
-export { splitThroughRegex } from './splitThroughRegex';
-export { vmLoader } from './virtualMachineLoader';
-export { webpackCompiler } from './webpackLoader';
-export {
-  filterSpecialAction, filterSpecialPlatform
-} from './streamPraseUtils';
