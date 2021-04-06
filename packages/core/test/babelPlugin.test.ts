3fdfc466e3d490cff6eac373a2911cf3f84ce49d
langyo <langyo.china@gmail.com>
2021-04-06 17:07:22 +0800

    :zap: Write some useful tests.

--- packages/core/test/babelPlugin.test.ts
+++ packages/core/test/babelPlugin.test.ts
@@ -1,5 +1,44 @@
-import plugin from '../src/babelPlugin';
+import { transformAsync } from '@babel/core';
+import { join } from 'path';
 
-test('Environment filter', () => {
-  expect(plugin).not.toBeUndefined();
+describe('Environment filter', () => {
+  test('On top, same environment', async () => {
+    const source = await transformAsync(`
+      'on client';
+      let i = 1;
+    `, {
+      plugins: [
+        [
+          join(__dirname, '../src/babelPlugin.ts'),
+          { targetEnv: 'client' }
+        ]
+      ],
+      ast: true
+    });
+    const target = await transformAsync(`
+      'on client';
+      let i = 1;
+    `, { ast: true });
+    expect(source.ast).toEqual(target.ast);
+  });
+
+  test('On top, different environment', async () => {
+    const source = await transformAsync(`
+      'on server';
+      let i = 1;
+    `, {
+      plugins: [
+        [
+          join(__dirname, '../src/babelPlugin.ts'),
+          { targetEnv: 'client' }
+        ]
+      ],
+      ast: true
+    });
+    const target = await transformAsync(`
+      'on server';
+    `, { ast: true });
+    expect(source.ast).toEqual(target.ast);
+  });
 });
+
