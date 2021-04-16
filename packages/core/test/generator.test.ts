d9c205b5e2c3818e4bf894636611bc8557e55f66
langyo <langyo.china@gmail.com>
2021-04-17 04:10:58 +0800

    📈 Add some tests for transform context.

--- packages/core/test/generator.test.ts
+++ packages/core/test/generator.test.ts
@@ -271,3 +271,84 @@ describe('Multiple environment filter', () => {
     )).toEqual(removeLocationInfo(parse(target)));
   });
 });
+
+describe('Transform data between different platform', () => {
+  test('On function, transform closure', () => {
+    const source = `
+      'on client';
+      let i = 1;
+      function test() {
+        'on server';
+        i = 3;
+      }
+      test();
+    `;
+    const targetOnClient = `
+      var __remoteContext = require('nickelcat-action-preset').generateRemoteContext('client');
+      let i = 1;
+      function test() {
+        // Context 0
+        __remoteContext.connect('server', 0, {
+          closure: { i },
+          arguments
+        }).callback(function (context) {
+          i = context.closure.i;
+        });
+      }
+      test();
+    `;
+    const targetOnServer = `
+      var __remoteContext = require('nickelcat-action-preset').generateRemoteContext('server');
+      __remoteContext.register('test', 0, function (__remoteContext) {
+        // Context 0
+        var i = __remoteContext.closure.get('i');
+        i = 3;
+        __remoteContext.closure.set('i', i);
+      });
+    `;
+    expect(removeLocationInfo(
+      transform(source, 'client')
+    )).toEqual(removeLocationInfo(parse(targetOnClient)));
+    expect(removeLocationInfo(
+      transform(source, 'server')
+    )).toEqual(removeLocationInfo(parse(targetOnServer)));
+  });
+
+  test('On function, transform arguments', () => {
+    const source = `
+      'on client';
+      let i = 1;
+      function test(i) {
+        'on server';
+        return i + 3;
+      }
+      i = test(i);
+    `;
+    const targetOnClient = `
+      var __remoteContext = require('nickelcat-action-preset').generateRemoteContext('client');
+      let i = 1;
+      function test() {
+        // Context 0
+        __remoteContext.connect('server', 0, {
+          closure: {},
+          arguments
+        }).callback(function (context) { });
+      }
+      i = test(i);
+    `;
+    const targetOnServer = `
+      var __remoteContext = require('nickelcat-action-preset').generateRemoteContext('server');
+      __remoteContext.register('test', 0, function (__remoteContext) {
+        // Context 0
+        var i = __remoteContext.arguments.get(0);
+        __remoteContext.callback(i + 3);
+      });
+    `;
+    expect(removeLocationInfo(
+      transform(source, 'client')
+    )).toEqual(removeLocationInfo(parse(targetOnClient)));
+    expect(removeLocationInfo(
+      transform(source, 'server')
+    )).toEqual(removeLocationInfo(parse(targetOnServer)));
+  });
+});
