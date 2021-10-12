2c8150a1636a8f4bf70c631aaea65f440beea55e
伊欧 <langyo.china@gmail.com>
2021-10-12 22:07:31 +0800

    ⚰️ Abandon the decorator.

--- examples/basic-web-server/index.tsx
+++ examples/basic-web-server/index.tsx
@@ -2,50 +2,61 @@
 
 import React, { useState } from 'react';
 
-@native.browser('react')
-class ClientEntry {
-  @native.browser.inject('#root')
-  async render() {
-    const [val, setVal] = useState(
-      await native.to.remote.http().run(async () => {
-        return 1;
-      })
-    );
+$$.native.browser(
+  'react',
+  {
+    rootElement: '#root',
+    defaultRemote: 'web',
+  },
+  {
+    async render() {
+      const [val, setVal] = useState(
+        await $$.native.to.remote.http().run(async () => 1)
+      );
 
-    return (
-      <>
-        <button
-          onClick={async () =>
-            setVal(await native.to.remote.http().get('/test'))
-          }
-        >
-          {native.to.remote.http().run(async () => {
-            return 'test';
-          })}
-        </button>
-        <button
-          onClick={async () =>
-            setVal(
-              await native.to.remote.http().run(async () => {
-                return val + 1;
-              })
-            )
-          }
-        >
-          {val}
-        </button>
-      </>
-    );
+      return (
+        <>
+          <button
+            onClick={async () =>
+              setVal(await $$.native.to.remote.http('backend').get('/test'))
+            }
+          >
+            {$$.native.to.remote.http.run(async () => 'test')}
+          </button>
+          <button
+            onClick={async () =>
+              setVal(await $$.native.to.remote.http.run(async () => val + 1))
+            }
+          >
+            {val}
+          </button>
+        </>
+      );
+    },
   }
-}
+);
 
-@remote.http('koa')
-class Services {
-  @remote.http.get('/test')
-  static async test() {
-    return 'test';
+$$.remote.http(
+  'koa',
+  {
+    id: 'web',
+  },
+  {
+    async test() {
+      return 'response from the web';
+    },
   }
+);
 
-  @remote.http.get('/')
-  clientEntry = ClientEntry;
-}
+$$.remote.http(
+  'koa',
+  {
+    id: 'backend',
+    port: 9233,
+  },
+  {
+    async test() {
+      return 'response from the backend';
+    },
+  }
+);
--- src/type.d.ts
+++ src/type.d.ts
@@ -1,58 +1,72 @@
-declare namespace native {
-  function browser(type: 'react' | 'vue' | 'ejs'): (target: any) => any;
-  namespace browser {
-    function inject(domSelector: string): any;
-  }
+declare namespace $$ {
+  namespace native {
+    function browser(
+      type: 'react' | 'vue' | 'ejs',
+      config: {
+        rootElement?: string;
+        defaultRemote?: string;
+      },
+      methods: {
+        [key: string]: (...sth: any[]) => Promise<any>;
+      }
+    ): void;
 
-  namespace to {
-    namespace remote {
-      function http(id?: string): {
-        get(route: string): Promise<any>;
-        run(func: () => Promise<any>): Promise<any>;
-      };
-      function websocket(id?: string): {
-        get(route: string): Promise<any>;
-        run(func: () => Promise<any>): Promise<any>;
-      };
-      function serverless(id?: string): {
-        get(route: string): Promise<any>;
-        run(func: () => Promise<any>): Promise<any>;
-      };
-      function hybrid(id?: string): {
-        get(route: string): Promise<any>;
-        run(func: () => Promise<any>): Promise<any>;
-      };
+    namespace to {
+      namespace remote {
+        function http(id?: string): {
+          get(route: string): Promise<any>;
+          run(func: () => Promise<any>): Promise<any>;
+        };
+        namespace http {
+          function get(route: string): Promise<any>;
+          function run(func: () => Promise<any>): Promise<any>;
+        }
+
+        function websocket(id?: string): {
+          get(route: string): Promise<any>;
+          run(func: () => Promise<any>): Promise<any>;
+        };
+        namespace websocket {
+          function get(route: string): Promise<any>;
+          function run(func: () => Promise<any>): Promise<any>;
+        }
+
+        function hybrid(id?: string): {
+          get(route: string): Promise<any>;
+          run(func: () => Promise<any>): Promise<any>;
+        };
+        namespace hybrid {
+          function get(route: string): Promise<any>;
+          function run(func: () => Promise<any>): Promise<any>;
+        }
+      }
     }
   }
-}
 
-declare namespace remote {
-  function http(
-    framework: 'express' | 'koa',
-    id?: string
-  ): (target: any) => any;
-  namespace http {
-    function get(route: string): any;
-  }
+  namespace remote {
+    function http(
+      framework: 'express' | 'koa',
+      config: {
+        id?: string;
+        port?: number;
+      },
+      methods: {}
+    ): void;
+    namespace http {
+      function get(route: string): any;
+    }
 
-  function websocket(id?: string): (target: any) => any;
-  namespace websocket {
-    function get(route: string): any;
-  }
+    function websocket(id?: string): (target: any) => any;
+    namespace websocket {
+      function get(route: string): any;
+    }
 
-  function serverless(
-    framework: 'aliyun-fc' | 'tencent-scf',
-    id?: string
-  ): (target: any) => any;
-  namespace serverless {
-    function get(route: string): any;
-  }
-
-  function hybrid(
-    framework: 'electron' | 'cordova',
-    id?: string
-  ): (target: any) => any;
-  namespace hybrid {
-    function get(route: string): any;
+    function hybrid(
+      framework: 'electron' | 'cordova',
+      id?: string
+    ): (target: any) => any;
+    namespace hybrid {
+      function get(route: string): any;
+    }
   }
 }
